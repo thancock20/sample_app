5bed945719a9650ba9d1cbfff5f012d8c856bbf2
Todd Hancock <thancock20@hotmail.com>
2016-07-23 00:58:11 -0500

    Fix typos in microposts controller and partial

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,7 +3,7 @@ class MicropostsController < ApplicationController
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
-    if @micrpost.save
+    if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
