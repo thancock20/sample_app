3b19416fe097e65eb6cd021b01936d72dad6badb
Todd Hancock <thancock20@hotmail.com>
2016-07-27 12:41:26 -0500

    Add follow and unfollow buttons to user pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 'static_pages#home'
   get '/help', to:    'static_pages#help'
   get '/about', to:   'static_pages#about'
@@ -20,4 +16,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
